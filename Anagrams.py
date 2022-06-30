abf4df0a4b1003818f687a7c28942d89e34d8564
21MH1A05A8 <97426569+21MH1A05A8@users.noreply.github.com>
2022-06-30 23:43:41 +0530

    n=input() n=n.lower() m=input() m=m.lower() k=list(n) s=set(m) f=0 for i in k:     if i not in s:         f+=1 if f==0:     print(True) else:     print(False)

--- Anagrams.py
+++ Anagrams.py
@@ -1,12 +1,14 @@
-s=input()
-n=s.casefold()
-k=input()
-m=k.casefold()
+n=input()
+n=n.lower()
+m=input()
+m=m.lower()
+k=list(n)
+s=set(m)
 f=0
-for i in n:
-    if i not in m:
+for i in k:
+    if i not in s:
         f+=1
 if f==0:
-    print('True')
+    print(True)
 else:
-    print('False')
+    print(False)
