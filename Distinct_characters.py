141decbd7b7041922254c4c7024f05f681738c67
21MH1A05A8 <97426569+21MH1A05A8@users.noreply.github.com>
2022-10-09 19:01:42 +0530

    n=input() n=n.lower() n=list(n) r=[] for i in n:     if i!=" ":         if i not in r:             r.append(i) r=sorted(r) print("".join(r))

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,11 +1,10 @@
 n=input()
 n=n.lower()
 n=list(n)
-c=0
 r=[]
 for i in n:
     if i!=" ":
-        if n.count(i)==1:
+        if i not in r:
             r.append(i)
-s=sorted(r)
-print("".join(s))
+r=sorted(r)
+print("".join(r))
