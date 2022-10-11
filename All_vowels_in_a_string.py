9e46806ed1754bd6b883c7f4b0ab7da3b28f636e
21MH1A05A8 <97426569+21MH1A05A8@users.noreply.github.com>
2022-10-11 16:06:13 +0530

    n=input() v='aeiou' s='AEIOU' c,p=0,0 v1=[] v2=[] for i in n:     if i!=" ":         if i in v:             if i not in v1:                 v1.append(i)         elif i in s:             if i not in v2:                 v2.append(i) if len(v1)==5 or len(v2)==5:     print(True) else:     print(False)

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,11 +1,19 @@
 n=input()
-v="aeiou"
-r=[]
+v='aeiou'
+s='AEIOU'
+c,p=0,0
+v1=[]
+v2=[]
 for i in n:
-    if i in v:
-        if i not in r:
-            r.append(i)
-if len(r)==5:
+    if i!=" ":
+        if i in v:
+            if i not in v1:
+                v1.append(i)
+        elif i in s:
+            if i not in v2:
+                v2.append(i)
+if len(v1)==5 or len(v2)==5:
     print(True)
 else:
     print(False)
+        
